59c72bbbd235f3818095af75883612e1efebd366
Michael Kazembe <122834780+MichaelKazembe@users.noreply.github.com>
2023-06-27 16:51:24 +0200

    Update 0-bubble_sort.c
    updated code

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -24,18 +24,27 @@ void swap(int *swap1, int *swap2)
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
+	int swapped;
+
+	if (size < 2 || array == NULL)
+		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
+		swapped = 0;
+		
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				swap(&array[j], &array[j + 1]);
 				print_array(array, size);
+				swapped = 1;
 
 			}
 		}
+		if (swapped == 0)
+			break;
 
 	}
 }
